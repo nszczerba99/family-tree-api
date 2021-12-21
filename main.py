d1730be25afec21d8ae054230c8246cc6fd6c0ad
nszczerba99 <nataliaszczerba00@gmail.com>
2021-12-21 05:21:25 +0100

    Cross origin fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import os
 from flask import Flask, request
 from neo4j import GraphDatabase
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from json import dumps
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ auth = (os.environ['USER'], os.environ['PASSWORD'])
 driver = GraphDatabase.driver(uri, auth=auth)
 
 @app.route('/api/family/members', methods=['GET'])
+@cross_origin()
 def get_family_memebers():
 	with driver.session() as session:
 		people_query_res = session.run('MATCH (p:Person) RETURN p.id AS id, p.name AS name, p.surname as surname')
@@ -21,6 +22,7 @@ def get_family_memebers():
 		return dumps(people)
 
 @app.route('/api/spouses', methods=['GET'])
+@cross_origin()
 def get_spouses():
 	with driver.session() as session:
 		spouse_relations_query_res = session.run('MATCH (p1:Person)-[r: SPOUSE]->(p2:Person) RETURN p1, p2')
@@ -28,6 +30,7 @@ def get_spouses():
 		return dumps(spouse_relations)
 
 @app.route('/api/familytree', methods=['GET'])
+@cross_origin()
 def get_family_tree():
 	with driver.session() as session:
 		def get_tree_for_person(person):
@@ -63,6 +66,7 @@ def get_family_tree():
 		return dumps(response)
 
 @app.route('/api/family/relation', methods=['GET'])
+@cross_origin()
 def get_relationship():
 	id1 = request.args.get('id1')
 	id2 = request.args.get('id2')
@@ -78,6 +82,7 @@ def get_relationship():
 		return dumps(relation_path)
 
 @app.route('/api/add/family/member', methods=['POST'])
+@cross_origin()
 def add_family_member():
 	new_member = request.get_json()
 
@@ -90,6 +95,7 @@ def add_family_member():
 		return str(id)
 
 @app.route('/api/add/family/relation', methods=['POST'])
+@cross_origin()
 def add_family_relation():
 	new_relation = request.get_json()
 
